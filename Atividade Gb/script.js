c9849e527c37c72f4ca9429e165d77c11167bbed
BryaHGRoc <127855127+BryanHGRoc@users.noreply.github.com>
2023-05-16 22:14:29 -0300

    fizemos o README e estamos terminando o trabalho

--- Atividade Gb/script.js
+++ Atividade Gb/script.js
@@ -1,18 +1,20 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+canvas.width  = 1000
+canvas.height = 1000
 
 //QUADRADO//
     ctx.fillStyle = 'blue';
-    ctx.fillRect(50,50,150,150);
+    ctx.fillRect(65,250,110,110);
 
 //RETANGULO//
     ctx.fillStyle = 'red';
-    ctx.fillRect(100,100,50,50);
+    ctx.fillRect(65,100,110,50);
 
 //LINHA//    
     ctx.beginPath();
-    ctx.moveTo(100,100);
-    ctx.lineTo(200, 100);
+    ctx.moveTo(300,100);
+    ctx.lineTo(400,100);
     ctx.stroke();
 
 //CURVA//
@@ -23,11 +25,11 @@ const ctx = canvas.getContext('2d');
 
 //CIRCULO//
     ctx.beginPath();
-    ctx.arc(100, 100, 50, 0, Math.PI * 2, true);
+    ctx.arc(350,300,50,0, Math.PI * 2, true);
+    ctx.moveTo(110,75);
+    ctx.stroke();
 
 //IMAGEM//
-    var imagem = new image();
-    imagem.onload =function() {
-        canvas.drawImage(imagem,50,38);
-    };
-    imagem.src = 'img/capi.jpg';
+   var imagem = document.getElementById('img2');
+
+ 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Atividades-2-Bimestre
  Atividades do segundo bimestre da dupla Gabriel Toledo D. e Bryan Henrique G. R. 
+ 
+  Trabalho de desenhar formas com canvas
+
+  fizemos um outro em sala mas por erros não conseguimos utiliza-lo, então esse foi feito, por isso a falta de comentarios no README.
